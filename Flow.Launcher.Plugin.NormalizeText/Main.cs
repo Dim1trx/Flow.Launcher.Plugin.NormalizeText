096819d8c31a4c6225694ecd9239f62617803470
yagodr <y.rodr1gues0811@outlook.com>
2025-05-07 23:58:03 -0300

    feat: add icon support and update plugin configuration

--- Flow.Launcher.Plugin.NormalizeText/Main.cs
+++ Flow.Launcher.Plugin.NormalizeText/Main.cs
@@ -1,21 +1,46 @@
 using System;
 using System.Collections.Generic;
-using Flow.Launcher.Plugin;
+using System.Globalization;
 
 namespace Flow.Launcher.Plugin.NormalizeText
 {
     public class NormalizeText : IPlugin
     {
         private PluginInitContext _context;
+        private TextInfo _textInfo;
 
         public void Init(PluginInitContext context)
         {
             _context = context;
+            _textInfo = CultureInfo.CurrentCulture.TextInfo;
         }
 
         public List<Result> Query(Query query)
         {
-            return new List<Result>();
+            try
+            {
+                var input = query.Search;
+                var titleResult = _textInfo.ToTitleCase(query.Search.ToLowerInvariant());
+
+                var result = new Result
+                {
+                    Title = "Normalize text",
+                    SubTitle = "Result: " + titleResult,
+                    Action = c =>
+                    {
+                        _context.API.CopyToClipboard(titleResult, showDefaultNotification: false);
+                        return true;
+                    },
+                    IcoPath = "./Images/icon.png",
+                };
+
+                return new List<Result> { result };
+            }
+            catch (Exception ex)
+            {
+                _context.API.ShowMsgError("Error", ex.Message);
+                return new List<Result> { };
+            }
         }
     }
 }
